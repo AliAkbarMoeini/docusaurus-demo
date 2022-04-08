cece945f874a8bc0ce11be648663e05d9bc32127
AliAkbar <AliAkbarMoeini@yahoo.com>
2022-04-08 15:51:07 +0430

    set required configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://aliakbarmoeini.github.io',
+  baseUrl: '/docusaurus-demo/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'aliakbarmoeini', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-demo', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   i18n: {
     defaultLocale: 'fa',
